d56cadceed1cd1ebb30174238eddf77dd0693449
Z4yne3 <153258881+hizikikun@users.noreply.github.com>
2025-11-05 09:27:30 +0900

    start_github_auto_commitファイル

--- batch_files/start_github_auto_commit.bat
+++ batch_files/start_github_auto_commit.bat
@@ -3,4 +3,4 @@ echo Starting Integrated GitHub Auto-Commit System...
 echo This will monitor code changes and automatically commit to GitHub
 echo.
 python integrated_github_system.py
-pause
+pause
