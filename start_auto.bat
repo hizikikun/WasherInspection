665c139bc0a4ea73a7e0e14c0b0eb6f03baa9c14
Z4yne3 <153258881+hizikikun@users.noreply.github.com>
2025-11-05 09:27:54 +0900

    start_autoファイル

--- start_auto.bat
+++ start_auto.bat
@@ -3,4 +3,4 @@ echo Starting Auto-Detection Mode...
 echo System will automatically detect if it's desktop or notebook
 echo.
 python network_washer_inspection_system.py auto
-pause
+pause
