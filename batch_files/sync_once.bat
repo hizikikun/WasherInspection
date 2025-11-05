499ac9a6ec54341de4511a0d54e5812e336f9f38
Z4yne3 <153258881+hizikikun@users.noreply.github.com>
2025-11-05 09:27:30 +0900

    sync_onceファイル

--- batch_files/sync_once.bat
+++ batch_files/sync_once.bat
@@ -3,4 +3,4 @@ echo Running one-time synchronization...
 echo This will sync code changes and training data once
 echo.
 python integrated_auto_sync.py once
-pause
+pause
