b63d08d9771f9b99f014bd51a5fccb63b4572e68
Z4yne3 <153258881+hizikikun@users.noreply.github.com>
2025-11-05 09:21:11 +0900

    auto_create_tokenファイル

--- auto_create_token.bat
+++ auto_create_token.bat
@@ -3,4 +3,4 @@ echo Auto GitHub Token Creator...
 echo This will automatically create a GitHub token for Cursor integration
 echo.
 python unified_token_creator.py
-pause
+pause
