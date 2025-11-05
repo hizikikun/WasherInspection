892c812ecc826655fa1aec4cda81e2a414be5b1b
Z4yne3 <153258881+hizikikun@users.noreply.github.com>
2025-11-05 09:27:55 +0900

    start_desktop_serverファイル

--- start_desktop_server.bat
+++ start_desktop_server.bat
@@ -3,4 +3,4 @@ echo Starting Desktop PC Server Mode...
 echo This will start the WebSocket server for notebook clients to connect
 echo.
 python network_washer_inspection_system.py server
-pause
+pause
