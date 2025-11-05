fda64c5485a3281558bbb649d78cfdddcc870fe2
Z4yne3 <153258881+hizikikun@users.noreply.github.com>
2025-11-05 09:27:56 +0900

    start_notebook_clientファイル

--- start_notebook_client.bat
+++ start_notebook_client.bat
@@ -4,4 +4,4 @@ echo This will connect to the desktop PC server
 echo Make sure the desktop PC server is running first!
 echo.
 python network_washer_inspection_system.py client
-pause
+pause
